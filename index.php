449bface09b176c0c7aa6b9f1478b9b7591cd686
mgbell <35670218+mgbell@users.noreply.github.com>
2020-06-06 00:56:54 +0100

    Add second card, adjust images

--- index.php
+++ index.php
@@ -14,17 +14,25 @@ include "./includes/parts/page.php";
 
 ?>
 
+<!-- Intro title and text -->
 <div class="container">
     <div class="row">
         <h2 class="grey-text text-darken-2 center">Welcome to mbFit</h2>
         <div class="row"></div>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum eligendi qui culpa magni architecto iure quas vel natus?</p>
     </div>
+</div>
+
+<!-- cards -->
+<div class="container">
+
     <div class="row">
+
+		<!-- card 1 -->
         <div class="row">
             <div class="card">
                 <div class="card-image">
-                    <img height="80px;" src="images/sample-1.jpg">
+                    <img height="180px;" src="images/background-1.jpg">
                     <span class="card-title">Bodyweight</span>
                 </div>
                 <div class="card-content">
@@ -36,5 +44,24 @@ include "./includes/parts/page.php";
                 </div>
             </div>
         </div>
+
+		<!-- card 2 -->
+        <div class="row">
+            <div class="card">
+                <div class="card-image">
+                    <img height="180px;" src="images/background-2.jpg">
+                    <span class="card-title">Cardio</span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#" class="black-text">Explore workouts</a>
+                </div>
+            </div>
+        </div>
+
     </div>
+
 </div>
