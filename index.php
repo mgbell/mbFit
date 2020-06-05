082a99632c0185581b36e1dbfcffebb541c3222f
mgbell <35670218+mgbell@users.noreply.github.com>
2020-06-06 00:45:13 +0100

    Add first card to homepage

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  
 
 /* HOME PAGE */ 
-$pageTitle = "Home";
+$pageTitle = "mbFit";
 
 ?>
 
@@ -13,3 +13,27 @@ $pageTitle = "Home";
 include "./includes/parts/page.php"; 
 
 ?>
+
+<div class="container">
+    <div class="row">
+        <h2 class="grey-text text-darken-2">Welcome to mbFit!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum eligendi qui culpa magni architecto iure quas vel natus?</p>
+    </div>
+    <div class="row">
+        <div class="row">
+            <div class="card">
+                <div class="card-image">
+                    <img height="80px;" src="images/sample-1.jpg">
+                    <span class="card-title">Bodyweight</span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#" class="black-text">Explore workouts</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
